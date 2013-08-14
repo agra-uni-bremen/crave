8c93ca9302f48b18cab1b082d8a2658fccec2896
Hoang M. Le <hle@informatik.uni-bremen.de>
2013-08-14 13:32:58 +0200

    fixed SystemC Data

--- src/crave/SystemC.hpp
+++ src/crave/SystemC.hpp
@@ -21,56 +21,6 @@ namespace crave {
   struct bitsize_traits< T, typename boost::enable_if< is_sysc_dt<T> >::type >
   : sc_dt_width<T>
   {};
-
-
-
-  template<typename T> struct AssignResult;
-  extern boost::function0<bool> random_bit;
-
-  template<typename T>
-  inline void _AssignSCDT(T & value, std::string const & s)
-  {
-    std::string::const_reverse_iterator it = s.rbegin();
-    for (unsigned i = 0; i < s.size(); i++) {
-      switch (*it) {
-        case '0':
-          value[i] = false;
-          break;
-        case '1':
-          value[i] = true;
-          break;
-        default:
-          value[i] = random_bit();
-      }
-      ++it;
-    }
-  }
-
-  template <int N>
-  struct AssignResult<sc_dt::sc_uint<N> > {
-    void operator() (sc_dt::sc_uint<N> & value, std::string const & s)
-    {
-      _AssignSCDT(value, s);
-    }
-  };
-
-
-  template <int N>
-  struct AssignResult<sc_dt::sc_int<N> > {
-    void operator() (sc_dt::sc_int<N> & value, std::string const & s)
-    {
-      _AssignSCDT(value, s);
-    }
-  };
-
-  template <int N>
-  struct AssignResult<sc_dt::sc_bv<N> > {
-    void operator() (sc_dt::sc_bv<N> & value, std::string const & s)
-    {
-      _AssignSCDT(value, s);
-    }
-  };
-
 } // namespace crave
 
 //  vim: ft=cpp:ts=2:sw=2:expandtab
