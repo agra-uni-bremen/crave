c9dc8a6fc25070430160a45022ae184ba9b10a56
Tino Flenker <flenkium@informatik.uni-bremen.de>
2013-08-18 17:33:46 +0200

    add possibility to activate or deactivate various solver backends.

--- src/lib/expression/metaSMTNodeVisitor.cpp
+++ src/lib/expression/metaSMTNodeVisitor.cpp
@@ -2,24 +2,81 @@
 #include "../../crave/expression/FactoryMetaSMT.hpp"
 #include "metaSMTNodeVisitorImpl.hpp"
 
-#include <metaSMT/backend/Boolector.hpp>
-#include <metaSMT/backend/SWORD_Backend.hpp>
-#include <metaSMT/backend/Z3_Backend.hpp>
-#include <metaSMT/backend/CUDD_Distributed.hpp>
+#ifdef metaSMT_USE_Boolector
+  #include <metaSMT/backend/Boolector.hpp>
+#endif
+#ifdef metaSMT_USE_SWORD
+  #include <metaSMT/backend/SWORD_Backend.hpp>
+#endif
+#ifdef metaSMT_USE_Z3
+  #include <metaSMT/backend/Z3_Backend.hpp>
+#endif
+#ifdef metaSMT_USE_CUDD
+  #include <metaSMT/backend/CUDD_Distributed.hpp>
+#endif
 
 #include <metaSMT/BitBlast.hpp>
 #include <metaSMT/DirectSolver_Context.hpp>
-#include <metaSMT/Priority_Context.hpp>
+
+#if defined(metaSMT_USE_CUDD) && defined(metaSMT_USE_Boolector)
+  #include <metaSMT/Priority_Context.hpp>
+#endif
+
 #include <metaSMT/UnpackFuture_Context.hpp>
 
 namespace crave {
 
-std::string metaSMTVisitor::solver_type = "Boolector";
+#ifdef metaSMT_USE_Boolector
+  std::string metaSMTVisitor::solver_type = "Boolector";
+#elif metaSMT_USE_Z3
+  std::string metaSMTVisitor::solver_type = "Z3";
+#elif metaSMT_USE_SWORD
+  std::string metaSMTVisitor::solver_type = "SWORD";
+#elif metaSMT_USE_CUDD
+  std::string metaSMTVisitor::solver_type = "Cudd";
+#endif
 
 void FactoryMetaSMT::setSolverType(std::string const& type) {
   metaSMTVisitor::solver_type = type;
 }
 
+#ifdef metaSMT_USE_SWORD
+typedef metaSMT::DirectSolver_Context <
+  metaSMT::UnpackFuture_Context <
+  metaSMT::solver::SWORD_Backend
+  >
+> SWORDSolverType;
+
+metaSMTVisitor* newVisitorSWORD() {
+  return new metaSMTVisitorImpl<SWORDSolverType>();
+}
+#endif
+
+#ifdef metaSMT_USE_Boolector
+typedef metaSMT::DirectSolver_Context <
+  metaSMT::UnpackFuture_Context <
+  metaSMT::solver::Boolector
+  >
+> BoolectorSolverType;
+
+metaSMTVisitor* newVisitorBoolector() {
+  return new metaSMTVisitorImpl<BoolectorSolverType>();
+}
+#endif
+
+#ifdef metaSMT_USE_Z3
+typedef metaSMT::DirectSolver_Context <
+  metaSMT::UnpackFuture_Context <
+  metaSMT::solver::Z3_Backend
+  >
+> Z3SolverType;
+
+metaSMTVisitor* newVisitorZ3() {
+  return new metaSMTVisitorImpl<Z3SolverType>();
+}
+#endif
+
+#ifdef metaSMT_USE_CUDD
 typedef metaSMT::DirectSolver_Context <
   metaSMT::UnpackFuture_Context <
   metaSMT::BitBlast <
@@ -27,56 +84,40 @@ typedef metaSMT::DirectSolver_Context <
   > >
 > CUDDSolverType;
 
-typedef metaSMT::DirectSolver_Context <
-  metaSMT::UnpackFuture_Context <
-  metaSMT::solver::SWORD_Backend
-  >
-> SWORDSolverType;
-
-typedef metaSMT::DirectSolver_Context <
-  metaSMT::UnpackFuture_Context <
-  metaSMT::solver::Boolector
-  >
-> BoolectorSolverType;
-
-typedef metaSMT::DirectSolver_Context <
-  metaSMT::UnpackFuture_Context <
-  metaSMT::solver::Z3_Backend
-  >
-> Z3SolverType;
-
-metaSMTVisitor* newVisitorSWORD() {
-  return new metaSMTVisitorImpl<SWORDSolverType>();
-}
-
-metaSMTVisitor* newVisitorBoolector() {
-  return new metaSMTVisitorImpl<BoolectorSolverType>();
-}
-
-metaSMTVisitor* newVisitorZ3() {
-  return new metaSMTVisitorImpl<Z3SolverType>();
-}
-
 metaSMTVisitor* newVisitorCudd() {
   return new metaSMTVisitorImpl<CUDDSolverType>();
 }
+#endif
+
+#if defined(metaSMT_USE_CUDD) && defined(metaSMT_USE_Boolector)
+typedef metaSMT::Priority_Context< BoolectorSolverType, CUDDSolverType > SolverType;
 
 metaSMTVisitor* newVisitorPriority() {
-  typedef metaSMT::Priority_Context< BoolectorSolverType, CUDDSolverType > SolverType;
   return new metaSMTVisitorImpl<SolverType>();
 }
+#endif
 
 metaSMTVisitor* FactoryMetaSMT::getNewInstance() {
+#ifdef metaSMT_USE_SWORD
   if (0 == metaSMTVisitor::solver_type.compare("SWORD"))
     return newVisitorSWORD();
-  else if (0 == metaSMTVisitor::solver_type.compare("Boolector"))
+#endif
+#if defined metaSMT_USE_Boolector
+  if (0 == metaSMTVisitor::solver_type.compare("Boolector"))
     return newVisitorBoolector();
-  else if (0 == metaSMTVisitor::solver_type.compare("Z3"))
+#endif
+#ifdef metaSMT_USE_Z3
+  if (0 == metaSMTVisitor::solver_type.compare("Z3"))
     return newVisitorZ3();
-  else if (0 == metaSMTVisitor::solver_type.compare("Cudd"))
+#endif
+#ifdef metaSMT_USE_CUDD
+  if (0 == metaSMTVisitor::solver_type.compare("Cudd"))
     return newVisitorCudd();
-  else if (0 == metaSMTVisitor::solver_type.compare("Priority"))
+#endif
+#if defined(metaSMT_USE_CUDD) && defined(metaSMT_USE_Boolector)
+  if (0 == metaSMTVisitor::solver_type.compare("Priority"))
     return newVisitorPriority();
+#endif
   assert("Unsupported backend is choosen.");
   return NULL;
 }
