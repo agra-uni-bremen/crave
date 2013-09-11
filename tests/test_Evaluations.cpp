2196b9bb41edb5c405e045d862a37bf296aa70c4
Tino Flenker <flenkium@informatik.uni-bremen.de>
2013-09-12 00:21:17 +0200

    add tests for Evaluator

--- tests/test_Evaluations.cpp
+++ tests/test_Evaluations.cpp
@@ -1,6 +1,6 @@
 #include <boost/test/unit_test.hpp>
 
-#include <crave/expression/EvalVisitor.hpp>
+#include <crave/expression/Evaluator.hpp>
 
 #include <set>
 #include <iostream>
@@ -12,51 +12,96 @@ BOOST_FIXTURE_TEST_SUITE(Evaluations_t, Context_Fixture )
 
 BOOST_AUTO_TEST_CASE (logical_not_t1) {
   randv<unsigned int> a(0);
-  EvalVisitor::eval_map assignments;
+  Evaluator evaluator;
 
-  assignments[a().id()] = Constant(0, 32, false);
-  EvalVisitor evaluator(assignments);
+  evaluator.assign(a, 0u);
 
   BOOST_REQUIRE( evaluator.evaluate(!(a() != 0)) );
-  BOOST_CHECK( evaluator.get_result().value() );
+  BOOST_CHECK( evaluator.get_result<bool>() );
 
-  assignments[a().id()] = Constant(42, 32, false);
+  evaluator.assign(a, 42u);
 
-  BOOST_REQUIRE( evaluator.evaluate(!(a() == 0), assignments) );
-  BOOST_CHECK( evaluator.get_result().value() );
+  BOOST_REQUIRE( evaluator.evaluate(!(a() == 0)) );
+  BOOST_CHECK( evaluator.get_result<bool>() );
 }
 
 BOOST_AUTO_TEST_CASE (logical_not_t2) {
   randv<unsigned char> a(0);
   randv<unsigned int> b(0);
-  EvalVisitor::eval_map assignments;
-  EvalVisitor eval;
-
-  assignments[a().id()] = Constant(1, 8, false);
-  assignments[b().id()] = Constant(35, 32, false);
-  eval.set_evaluation_map(assignments);
-
+  Evaluator eval;
   NodePtr expression(eval.get_expression(
     if_then_else(!(a() % 2 == 0), b() > 0 && b() <= 50, b() > 50 && b() <= 100 )
   ));
-  BOOST_REQUIRE( eval.evaluate(*expression) );
-  BOOST_CHECK( eval.get_result().value() );
 
-  assignments[a().id()] = Constant(2, 8, false);
-  assignments[b().id()] = Constant(75, 32, false);
+  eval.assign(a, 1u);
 
-  BOOST_REQUIRE( eval.evaluate(*expression, assignments) );
-  BOOST_CHECK( eval.get_result().value() );
-
-  assignments[a().id()] = Constant(1, 8, false);
-
-  BOOST_REQUIRE( eval.evaluate(*expression, assignments) );
-  BOOST_CHECK( !eval.get_result().value() );
-
-  assignments.erase(b().id());
-
-  eval.set_evaluation_map(assignments);
   BOOST_CHECK( !eval.evaluate(*expression) );
+
+  eval.assign(b, 35u);
+
+  BOOST_REQUIRE( eval.evaluate(*expression) );
+  BOOST_CHECK( eval.get_result<bool>() );
+
+  eval.assign(a, 2u);
+  eval.assign(b, 75u);
+
+  BOOST_REQUIRE( eval.evaluate(*expression) );
+  BOOST_CHECK( eval.get_result<bool>() );
+
+  eval.assign(a, 1u);
+
+  BOOST_REQUIRE( eval.evaluate(*expression) );
+  BOOST_CHECK( !eval.get_result<bool>() );
+}
+
+
+BOOST_AUTO_TEST_CASE (neg_t1) {
+  randv<int> a(0);
+  randv<int> b(0);
+  Evaluator eval;
+
+  eval.assign(a, 1337);
+  BOOST_CHECK(  eval.evaluate(-a() == 1337) );
+  BOOST_CHECK( !eval.get_result<bool>() );
+
+  eval.assign(b, -1337);
+  BOOST_CHECK( eval.evaluate(a() == -b()) );
+  BOOST_CHECK( eval.get_result<bool>() );
+}
+
+BOOST_AUTO_TEST_CASE (neg_t2) {
+  int a = 1337;
+  randv<int> b(0);
+  Evaluator eval;
+
+  eval.assign(b, -a);
+  BOOST_CHECK( eval.evaluate(b()) );
+  BOOST_CHECK_EQUAL( eval.get_result<int>(), -1337 );
+}
+
+BOOST_AUTO_TEST_CASE (complement_t1) {
+  randv<int> a(0);
+  randv<int> b(0);
+  Evaluator eval;
+
+  eval.assign(a, 0);
+  eval.assign(b, -1);
+  BOOST_CHECK( eval.evaluate(~a() == b()) );
+  BOOST_CHECK( eval.get_result<bool>() );
+}
+
+BOOST_AUTO_TEST_CASE (complement_t2) {
+  int a = 42;
+  randv<int> b(0);
+  Evaluator eval;
+
+  eval.assign(b, a);
+  BOOST_CHECK( eval.evaluate(~b()) );
+  BOOST_CHECK_EQUAL( eval.get_result<int>(), -43 );
+}
+
+
+
 }
 
 
