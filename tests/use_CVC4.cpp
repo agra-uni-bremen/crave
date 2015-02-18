70404d0c70819d56fe0ce8ee3ce4ad5020ec6f19
Hoang M. Le <hle@informatik.uni-bremen.de>
2015-02-18 13:58:32 +0100

    added CVC4 test

---
+++ tests/use_CVC4.cpp
@@ -0,0 +1,19 @@
+#define BOOST_TEST_MODULE CVC4
+#include <crave/ConstrainedRandom.hpp>
+
+#include <string>
+
+struct Context_Fixture {
+  Context_Fixture () {
+    crave::set_solver_backend("CVC4");
+  }
+};
+
+#include "test_Syntax.cpp"
+#include "test_Context.cpp"
+#include "test_Operators.cpp"
+#include "test_Random_Object.cpp"
+#include "test_Constraint_Management.cpp"
+#include "test_Vector_Constraint.cpp"
+#include "test_Distribution.cpp"
+#include "test_Evaluations.cpp"
