082d74841472d138a137c602307becbd2b540550
Hoang M. Le <hle@informatik.uni-bremen.de>
2015-04-20 18:50:03 +0200

    moved solver-independent tests to a seperate executable

---
+++ tests/solver_independent_tests.cpp
@@ -0,0 +1,10 @@
+#define BOOST_TEST_MODULE solver_independent
+#include <crave/ConstrainedRandom.hpp>
+
+struct Context_Fixture {
+  Context_Fixture () {
+  }
+};
+
+#include "test_Syntax.cpp"
+#include "test_Evaluations.cpp"
--- tests/use_Cudd.cpp
+++ tests/use_Cudd.cpp
@@ -9,11 +9,10 @@ struct Context_Fixture {
   }
 };
 
-#include "test_Syntax.cpp"
 #include "test_Context.cpp"
 #include "test_Operators.cpp"
 #include "test_Random_Object.cpp"
 #include "test_Constraint_Management.cpp"
 #include "test_Vector_Constraint.cpp"
 #include "test_Distribution.cpp"
-#include "test_Evaluations.cpp"
+
