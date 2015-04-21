42fb47d0b71011fb35d9747695ace4e035999d5e
Hoang M. Le <hle@informatik.uni-bremen.de>
2015-04-21 14:10:13 +0200

    added test for mixing constraint and expression

--- tests/test_UserConstraint.cpp
+++ tests/test_UserConstraint.cpp
@@ -1,6 +1,7 @@
 #include <boost/test/unit_test.hpp>
 
 #include <crave/ir/UserConstraint.hpp>
+#include <crave/utils/Evaluator.hpp>
 
 //using namespace std;
 using namespace crave;
@@ -29,6 +30,31 @@ BOOST_AUTO_TEST_CASE( constraint_partitioning )
   cp.partition();
   BOOST_CHECK_EQUAL(cp.getPartitions().size(), 2);
 }
+
+BOOST_AUTO_TEST_CASE( constraint_expression_mixing )
+{
+  randv<unsigned> x, y, z, t;
+  expression e1 = make_expression(x() + y());
+  expression e2 = make_expression(z() + t() > 10);
+
+  Evaluator evaluator;
+  evaluator.assign(x(), 8);
+  evaluator.assign(y(), 1);
+  evaluator.assign(z(), 8);
+  evaluator.assign(t(), 3);   
+
+  BOOST_REQUIRE( evaluator.evaluate((x() > z()) && (e1 <= 10) && e2) );
+  BOOST_CHECK( ! evaluator.result<bool>() );
+
+  evaluator.assign(x(), 9);
+
+  BOOST_REQUIRE( evaluator.evaluate((x() > z()) && (e1 + 1 <= 11) && e2) );
+  BOOST_CHECK( evaluator.result<bool>() );
+
+  BOOST_REQUIRE( evaluator.evaluate(e1 * e1) );
+  BOOST_CHECK_EQUAL( evaluator.result<unsigned>(), 100);
+}
+
 
 BOOST_AUTO_TEST_SUITE_END() // Syntax
 
