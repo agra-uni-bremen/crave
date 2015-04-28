59b79ecddae38b10babe57ecbbe79e04b1c3fa91
Hoang M. Le <hle@informatik.uni-bremen.de>
2015-04-28 19:34:14 +0200

    added ; after MACROs for correct formatting by clang-format

--- src/crave/frontend/RandomBase.hpp
+++ src/crave/frontend/RandomBase.hpp
@@ -134,30 +134,32 @@ class randv_base : public rand_base {
 
 template <>
 class randv<bool> : public randv_base<bool> {
-  RANDV_COMMON_INTERFACE(bool)
-  RANDV_PRIM_INTERFACE(bool)
-  RANDV_BITWISE_INTERFACE(bool)
+  RANDV_COMMON_INTERFACE(bool);
+  RANDV_PRIM_INTERFACE(bool);
+  RANDV_BITWISE_INTERFACE(bool);
 };
 
 // for all C/C++ built-in integer types
-#define RANDV_INTEGER_TYPE(typename)                                                                     \
-  template <>                                                                                            \
-  class randv<typename> : public randv_base<typename> {                                                  \
-    RANDV_COMMON_INTERFACE(typename) RANDV_PRIM_INTERFACE(typename) RANDV_ARITHMETIC_INTERFACE(typename) \
-        RANDV_BITWISE_INTERFACE(typename)                                                                \
+#define RANDV_INTEGER_TYPE(typename)                    \
+  template <>                                           \
+  class randv<typename> : public randv_base<typename> { \
+    RANDV_COMMON_INTERFACE(typename);                   \
+    RANDV_PRIM_INTERFACE(typename);                     \
+    RANDV_ARITHMETIC_INTERFACE(typename);               \
+    RANDV_BITWISE_INTERFACE(typename);                  \
   };
 
-RANDV_INTEGER_TYPE(int)
-RANDV_INTEGER_TYPE(unsigned int)
-RANDV_INTEGER_TYPE(char)
-RANDV_INTEGER_TYPE(signed char)
-RANDV_INTEGER_TYPE(unsigned char)
-RANDV_INTEGER_TYPE(short)
-RANDV_INTEGER_TYPE(unsigned short)
-RANDV_INTEGER_TYPE(long)
-RANDV_INTEGER_TYPE(unsigned long)
-RANDV_INTEGER_TYPE(long long)
-RANDV_INTEGER_TYPE(unsigned long long)
+RANDV_INTEGER_TYPE(int);
+RANDV_INTEGER_TYPE(unsigned int);
+RANDV_INTEGER_TYPE(char);
+RANDV_INTEGER_TYPE(signed char);
+RANDV_INTEGER_TYPE(unsigned char);
+RANDV_INTEGER_TYPE(short);
+RANDV_INTEGER_TYPE(unsigned short);
+RANDV_INTEGER_TYPE(long);
+RANDV_INTEGER_TYPE(unsigned long);
+RANDV_INTEGER_TYPE(long long);
+RANDV_INTEGER_TYPE(unsigned long long);
 
 unsigned int default_rand_vec_size();
 
