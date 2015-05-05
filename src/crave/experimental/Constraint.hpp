c248e8e9128983999970342b26978b0daa26e133
Hoang M. Le <hle@informatik.uni-bremen.de>
2015-05-05 15:33:35 +0200

    updated crv_constraint to use expression_list

--- src/crave/experimental/Constraint.hpp
+++ src/crave/experimental/Constraint.hpp
@@ -1,6 +1,9 @@
+// Copyright 2014 The CRAVE developers. All rights reserved.
+
 #pragma once
 
 #include "Object.hpp"
+#include "Expression.hpp"
 #include "../ir/UserExpression.hpp"
 
 #include <glog/logging.h>
@@ -11,44 +14,23 @@
 
 namespace crave {
 
-class crv_constraint;
-
 class crv_constraint : public crv_object {
  public:
   crv_constraint(crv_object_name) { }
 
-  template <typename... Exprs>
-  crv_constraint(crv_object_name, Exprs... exprs) : exprs_() { 
-    add_exprs(exprs...);
-  }
+  crv_constraint(crv_object_name, expression_list list) : list_(list) { }
 
-  template <typename... Exprs>
-  void operator()(Exprs... exprs) {
-    exprs_.clear();
-    add_exprs(exprs...);
-  }
+  template <typename Expr>
+  void operator()(Expr expr) { list_.add_expr(expr); }
+
+  expression single_expr() { return list_.single_expr(); }
+
+  void operator=(expression_list list) { list_ = list; }
 
   std::string kind() override { return "crv_constraint"; }
 
-  template <typename Expr>
-  void add_expr(Expr expr) { exprs_.push_back(make_expression(expr)); }
-
-  expression expr() {
-    expression result = value_to_expression(true);
-    for (auto e : exprs_) result = make_expression(result && e);
-    return result;
-  }
-
  private:
-  void add_exprs() { }
-
-  template <typename Expr, typename... Exprs>
-  void add_exprs(Expr expr, Exprs... exprs) { 
-    exprs_.push_back(make_expression(expr));
-    add_exprs(exprs...);
-  }
-
-  std::vector<expression> exprs_;  
+  expression_list list_;
 };
 
 }  // end namespace crave
