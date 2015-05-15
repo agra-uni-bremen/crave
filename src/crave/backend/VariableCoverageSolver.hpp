1bddaaa261408b6babd0ea01312c61826134d4e3
Niklas <nbruns@tzi.de>
2015-05-15 17:24:36 +0200

    stylefix VariableCoverageSolver.hpp

--- src/crave/backend/VariableCoverageSolver.hpp
+++ src/crave/backend/VariableCoverageSolver.hpp
@@ -2,37 +2,50 @@
 
 #pragma once
 
+#include <string>
+#include <set>
+
 #include "VariableSolver.hpp"
+
 
 namespace crave {
 
 struct VariableCoverageSolver : VariableSolver {
-
-  VariableCoverageSolver(VariableContainer& vcon, ConstraintPartition& cp) : VariableSolver(vcon, cp) {
-
+  VariableCoverageSolver(VariableContainer& vcon, ConstraintPartition& cp)
+  : VariableSolver(vcon, cp) {
     LOG(INFO) << "Create coverage solver for partition " << constr_pttn;
 
     BOOST_FOREACH(ConstraintPtr c, constr_pttn) {
-      if (c->isSoft()) continue;  // coverage solver ignores soft constraints for now
+      if (c->isSoft()) {
+          continue;  // coverage solver ignores soft constraints for now
+      }
       if (!c->isCover()) solver->makeAssertion(*c->expr());
     }
   }
 
   virtual bool solve() {
-
     BOOST_FOREACH(ConstraintPtr c, constr_pttn) {
       if (!c->isCover()) continue;
-      if (covered_set.find(c->name()) != covered_set.end()) continue;  // alread covered
+      if (covered_set.find(c->name()) != covered_set.end()) {
+          continue;  // alread covered
+      }
       // try solve
-      BOOST_FOREACH(VariableContainer::ReadRefPair pair, var_ctn.read_references) {
-        if (constr_pttn.containsVar(pair.first)) solver->makeAssumption(*pair.second->expr());
+      BOOST_FOREACH(VariableContainer::ReadRefPair pair,
+                    var_ctn.read_references) {
+        if (constr_pttn.containsVar(pair.first)) {
+            solver->makeAssumption(*pair.second->expr());
+        }
       }
       solver->makeAssumption(*c->expr());
       if (solver->solve()) {
-        LOG(INFO) << "Solve partition " << constr_pttn << " hitting constraint " << c->name();
+        LOG(INFO) << "Solve partition " << constr_pttn
+                  << " hitting constraint " << c->name();
         covered_set.insert(c->name());
-        BOOST_FOREACH(VariableContainer::WriteRefPair pair, var_ctn.write_references) {
-          if (constr_pttn.containsVar(pair.first)) solver->read(*var_ctn.variables[pair.first], *pair.second);
+        BOOST_FOREACH(VariableContainer::WriteRefPair pair,
+                      var_ctn.write_references) {
+          if (constr_pttn.containsVar(pair.first)) {
+              solver->read(*var_ctn.variables[pair.first], *pair.second);
+          }
         }
         return true;
       }
@@ -43,4 +56,4 @@ struct VariableCoverageSolver : VariableSolver {
  private:
   std::set<std::string> covered_set;
 };
-}
+}  // namespace crave
