1e455ce009f67f4092bfbec6bb464a8e57dc5966
Niklas <nbruns@tzi.de>
2015-05-16 16:46:50 +0200

    stylefix EvalVisitor.hpp

--- src/crave/ir/visitor/EvalVisitor.hpp
+++ src/crave/ir/visitor/EvalVisitor.hpp
@@ -2,23 +2,28 @@
 
 #pragma once
 
-#include "../Node.hpp"
-#include "NodeVisitor.hpp"
-
 #include <map>
 #include <stack>
 #include <utility>
 
+#include "../Node.hpp"
+#include "NodeVisitor.hpp"
+
+
+
 namespace crave {
 
 class EvalVisitor : NodeVisitor {
-
   typedef std::pair<Constant, bool> stack_entry;
 
  public:
   typedef std::map<int, Constant> eval_map;
 
-  EvalVisitor(eval_map& m) : NodeVisitor(), exprStack_(), evalMap_(m), result_() {}
+  explicit EvalVisitor(eval_map& m) :
+  NodeVisitor(),
+  exprStack_(),
+  evalMap_(m),
+  result_() {}
 
  private:
   virtual void visitNode(Node const&);
@@ -64,13 +69,13 @@ class EvalVisitor : NodeVisitor {
   void pop2(stack_entry&, stack_entry&);
   void pop3(stack_entry&, stack_entry&, stack_entry&);
   void evalBinExpr(BinaryExpression const&, stack_entry&, stack_entry&);
-  void evalTernExpr(TernaryExpression const&, stack_entry&, stack_entry&, stack_entry&);
+  void evalTernExpr(TernaryExpression const&, stack_entry&,
+                    stack_entry&, stack_entry&);
 
  public:
   Constant result() const { return result_; }
 
   bool evaluate(Node const& expr) {
-
     expr.visit(*this);
     stack_entry entry;
     pop(entry);
@@ -98,7 +103,8 @@ inline void EvalVisitor::pop2(stack_entry& fst, stack_entry& snd) {
   snd = exprStack_.top();
   exprStack_.pop();
 }
-inline void EvalVisitor::pop3(stack_entry& fst, stack_entry& snd, stack_entry& trd) {
+inline void EvalVisitor::pop3(stack_entry& fst,
+                              stack_entry& snd, stack_entry& trd) {
   assert(exprStack_.size() >= 3);
   fst = exprStack_.top();
   exprStack_.pop();
@@ -107,11 +113,13 @@ inline void EvalVisitor::pop3(stack_entry& fst, stack_entry& snd, stack_entry& t
   trd = exprStack_.top();
   exprStack_.pop();
 }
-inline void EvalVisitor::evalBinExpr(BinaryExpression const& bin, stack_entry& fst, stack_entry& snd) {
+inline void EvalVisitor::evalBinExpr(BinaryExpression const& bin,
+                                     stack_entry& fst, stack_entry& snd) {
   visitBinaryExpr(bin);
   pop2(snd, fst);
 }
-inline void EvalVisitor::evalTernExpr(TernaryExpression const& tern, stack_entry& fst, stack_entry& snd,
+inline void EvalVisitor::evalTernExpr(TernaryExpression const& tern,
+                                      stack_entry& fst, stack_entry& snd,
                                       stack_entry& trd) {
   visitTernaryExpr(tern);
   pop3(trd, snd, fst);
