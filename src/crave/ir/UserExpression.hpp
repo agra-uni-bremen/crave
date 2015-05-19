e4d19943ca6074ef3094c2bf50475518b988db2b
Niklas <nbruns@tzi.de>
2015-05-19 21:32:52 +0200

    stylefix UserExpression.hpp

--- src/crave/ir/UserExpression.hpp
+++ src/crave/ir/UserExpression.hpp
@@ -1,3 +1,4 @@
+// Copyright 2014 The CRAVE developers. All rights reserved.//
 #pragma once
 
 #include "Context.hpp"
@@ -5,23 +6,27 @@
 
 namespace crave {
 
+using boost::proto::tag::terminal;
+using boost::proto::result_of::make_expr;
+
 struct expr_tag : public Constraint<boost::proto::terminal<NodePtr>::type> {
   typedef Constraint<boost::proto::terminal<NodePtr>::type> base_type;
-  expr_tag(NodePtr n) : base_type(boost::proto::make_expr<boost::proto::tag::terminal>(n)) {}
+  explicit expr_tag(NodePtr n) :
+  base_type(boost::proto::make_expr<terminal>(n)) {}
 };
 
-typedef typename boost::proto::result_of::make_expr<boost::proto::tag::terminal, Constraint_Domain, NodePtr>::type
+typedef typename make_expr<terminal, Constraint_Domain, NodePtr>::type
     expression;
 
 template <typename Expr>
 expression make_expression(Expr e) {
   static Context ctx(&crave::variables);
-  return boost::proto::make_expr<boost::proto::tag::terminal, Constraint_Domain>(boost::proto::eval(e, ctx));
+  return boost::proto::make_expr<terminal, Constraint_Domain>(boost::proto::eval(e, ctx));
 }
 
 template <typename Integer>
 expression value_to_expression(Integer const& i) {
-  return boost::proto::make_expr<boost::proto::tag::terminal, Constraint_Domain>(Context::new_value(i));
+  return boost::proto::make_expr<terminal, Constraint_Domain>(Context::new_value(i));
 }
 
 }  // end namespace crave
