aa0fdcc1ed5a7038561c23b46d7f9dc0a605b409
Niklas <nbruns@tzi.de>
2015-05-19 13:03:13 +0200

    stylefix ReadReference

--- src/crave/frontend/ReadReference.hpp
+++ src/crave/frontend/ReadReference.hpp
@@ -5,21 +5,26 @@
 #include "ConstraintType.hpp"
 #include "Variable.hpp"
 
+using boost::proto::terminal;
+using boost::proto::make_expr;
+
 namespace crave {
 // ReadReference
 
 template <typename value_type>
 struct read_ref_tag : public var_tag<value_type> {
-  read_ref_tag(int id_, value_type const& ref_) : var_tag<value_type>(id_), ref(ref_) {}
+  read_ref_tag(int id_, value_type const& ref_) :
+  var_tag<value_type>(id_), ref(ref_) {}
+
   value_type const& ref;
 };
 
 template <typename value_type_>
-struct ReadReference : public Constraint<typename boost::proto::terminal<read_ref_tag<value_type_> >::type> {
-  typedef Constraint<typename boost::proto::terminal<read_ref_tag<value_type_> >::type> base_type;
-
-  explicit ReadReference(value_type_ const& ref)
-      : base_type(boost::proto::make_expr<boost::proto::tag::terminal>(read_ref_tag<value_type_>(new_var_id(), ref))) {}
+struct ReadReference :
+  public Constraint<typename terminal<read_ref_tag<value_type_>>::type> {
+    typedef Constraint<typename terminal<read_ref_tag<value_type_>>::type> base_type;
+    explicit ReadReference(value_type_ const& ref)
+      : base_type(make_expr<boost::proto::tag::terminal>(read_ref_tag<value_type_>(new_var_id(), ref))) {}
 
   typedef value_type_ value_type;
 
@@ -29,6 +34,6 @@ struct ReadReference : public Constraint<typename boost::proto::terminal<read_re
 template <typename T>
 typename boost::proto::result_of::make_expr<boost::proto::tag::terminal, Constraint_Domain, read_ref_tag<T> >::type
 reference(T const& ref) {
-  return boost::proto::make_expr<boost::proto::tag::terminal, Constraint_Domain>(read_ref_tag<T>(new_var_id(), ref));
+  return make_expr<boost::proto::tag::terminal, Constraint_Domain>(read_ref_tag<T>(new_var_id(), ref));
 }
-}
+}  // namespace crave
