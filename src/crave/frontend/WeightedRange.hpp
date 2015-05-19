87bbf0270b100c4c619427cc8180580742ad7bb9
Niklas <nbruns@tzi.de>
2015-05-19 13:12:33 +0200

    stylefix WeightedRange.hpp

--- src/crave/frontend/WeightedRange.hpp
+++ src/crave/frontend/WeightedRange.hpp
@@ -6,7 +6,8 @@ namespace crave {
 
 template <typename T>
 struct weighted_range {
-  weighted_range(T l, T r, unsigned w) : left(l), right(r), weight(w), accumWeight(0) {}
+  weighted_range(T l, T r, unsigned w) :
+  left(l), right(r), weight(w), accumWeight(0) {}
 
   bool operator<(const weighted_range<T>& other) const {
     if (left < other.left) return true;
@@ -36,4 +37,4 @@ template <typename T>
 struct weighted_value : weighted_range<T> {
   weighted_value(T v, unsigned w) : weighted_range<T>(v, v, w) {}
 };
-}
+}  // namespace crave
