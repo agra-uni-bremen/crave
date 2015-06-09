1a801946e0c21aa1878198720b743f994cbcdf1e
Niklas <nbruns@tzi.de>
2015-06-09 14:33:47 +0200

    delete redundant class

--- src/crave/ir/ConstraintPartitioner.hpp
+++ src/crave/ir/ConstraintPartitioner.hpp
@@ -9,7 +9,7 @@
 
 #include "Context.hpp"
 #include "ConstraintManager.hpp"
-/*
+
 namespace crave {
 
 typedef boost::shared_ptr<UserVectorConstraint> VectorConstraintPtr;
@@ -26,15 +26,14 @@ struct ConstraintPartitioner {
     LOG(INFO) << "Merge set " << mng.id_ << " with set(s)";
     BOOST_FOREACH(unsigned id, constr_mngs_) { LOG(INFO) << " " << id; }
     constr_mngs_.insert(mng.id_);
-
     BOOST_FOREACH(ConstraintPtr c, mng.constraints_) {
       if (c->isEnabled()) {
-        if (c->isVectorConstraint()) {
+        if (c->isVectorConstraint())
           vec_constraints_.push_back(
               boost::static_pointer_cast<UserVectorConstraint>(c));
-        } else {
+
+        else
           constraints_.push_back(c);
-        }
       }
     }
   }
@@ -42,7 +41,7 @@ struct ConstraintPartitioner {
   void partition() {
     while (!constraints_.empty()) {
       ConstraintPartition cp;
-      maximizePartition(cp, constraints_);
+      maximizePartition(&cp, &constraints_);
       partitions_.push_back(cp);
     }
     LOG(INFO) << "Partition results of set(s)";
@@ -50,14 +49,12 @@ struct ConstraintPartitioner {
     LOG(INFO) << ": ";
 
     LOG(INFO) << "  " << vec_constraints_.size() << " vector constraint(s):";
-
     BOOST_FOREACH(VectorConstraintPtr c, vec_constraints_) {
       LOG(INFO) << "   " << c->name();
     }
 
     LOG(INFO) << "  " << partitions_.size() << " constraint partition(s):";
     uint cnt = 0;
-
     BOOST_FOREACH(ConstraintPartition& cp, partitions_) {
       LOG(INFO) << "    #" << ++cnt << ": " << cp;
     }
@@ -107,9 +104,7 @@ struct ConstraintPartitioner {
   // results
   std::vector<ConstraintPartition> partitions_;
   std::vector<VectorConstraintPtr> vec_constraints_;
-};*/
-
-
+};
 
 
 }  // namespace crave
