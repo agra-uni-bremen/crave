2a90994e670262c773e94c1fc89989d16c5e9ebf
Niklas <nbruns@tzi.de>
2015-06-24 21:35:39 +0200

    added Interface

--- src/crave/ir/visitor/ComplexityEstimationVisitor.hpp
+++ src/crave/ir/visitor/ComplexityEstimationVisitor.hpp
@@ -1,15 +1,122 @@
 // Copyright 2014 The CRAVE developers. All rights reserved.//
 
 #pragma once
+#include <map>
+#include <stack>
+#include <utility>
+
+#include "../Node.hpp"
+#include "NodeVisitor.hpp"
 
 namespace crave {
 
     class ComplexityEstimationVisitor : NodeVisitor {
-    public:
-        ComplexityEstimationVisitor();
-        ComplexityEstimationVisitor(const ComplexityEstimationVisitor& orig);
-        virtual ~ComplexityEstimationVisitor();
-    private:
+        typedef std::pair<Constant, bool> stack_entry;
 
+    public:
+        typedef std::map<int, Constant> eval_map;
+
+        explicit ComplexityEstimationVisitor(eval_map* m) : NodeVisitor(), exprStack_(), evalMap_(m), result_() {
+        }
+
+    private:
+        virtual void visitNode(Node const&);
+        virtual void visitTerminal(Terminal const&);
+        virtual void visitUnaryExpr(UnaryExpression const&);
+        virtual void visitUnaryOpr(UnaryOperator const&);
+        virtual void visitBinaryExpr(BinaryExpression const&);
+        virtual void visitBinaryOpr(BinaryOperator const&);
+        virtual void visitTernaryExpr(TernaryExpression const&);
+        virtual void visitPlaceholder(Placeholder const&);
+        virtual void visitVariableExpr(VariableExpr const&);
+        virtual void visitConstant(Constant const&);
+        virtual void visitVectorExpr(VectorExpr const&);
+        virtual void visitNotOpr(NotOpr const&);
+        virtual void visitNegOpr(NegOpr const&);
+        virtual void visitComplementOpr(ComplementOpr const&);
+        virtual void visitInside(Inside const&);
+        virtual void visitExtendExpr(ExtendExpression const&);
+        virtual void visitAndOpr(AndOpr const&);
+        virtual void visitOrOpr(OrOpr const&);
+        virtual void visitLogicalAndOpr(LogicalAndOpr const&);
+        virtual void visitLogicalOrOpr(LogicalOrOpr const&);
+        virtual void visitXorOpr(XorOpr const&);
+        virtual void visitEqualOpr(EqualOpr const&);
+        virtual void visitNotEqualOpr(NotEqualOpr const&);
+        virtual void visitLessOpr(LessOpr const&);
+        virtual void visitLessEqualOpr(LessEqualOpr const&);
+        virtual void visitGreaterOpr(GreaterOpr const&);
+        virtual void visitGreaterEqualOpr(GreaterEqualOpr const&);
+        virtual void visitPlusOpr(PlusOpr const&);
+        virtual void visitMinusOpr(MinusOpr const&);
+        virtual void visitMultipliesOpr(MultipliesOpr const&);
+        virtual void visitDevideOpr(DevideOpr const&);
+        virtual void visitModuloOpr(ModuloOpr const&);
+        virtual void visitShiftLeftOpr(ShiftLeftOpr const&);
+        virtual void visitShiftRightOpr(ShiftRightOpr const&);
+        virtual void visitVectorAccess(VectorAccess const&);
+        virtual void visitIfThenElse(IfThenElse const&);
+        virtual void visitForEach(ForEach const&);
+        virtual void visitUnique(Unique const&);
+        virtual void visitBitslice(Bitslice const&);
+        void pop(stack_entry&);
+        void pop2(stack_entry&, stack_entry&);
+        void pop3(stack_entry&, stack_entry&, stack_entry&);
+        void evalBinExpr(BinaryExpression const&, stack_entry&, stack_entry&);
+        void evalTernExpr(TernaryExpression const&, stack_entry&, stack_entry&, stack_entry&);
+
+    public:
+
+        Constant result() const {
+            return result_;
+        }
+
+        bool evaluate(Node const& expr) {
+            expr.visit(this);
+            stack_entry entry;
+            pop(entry);
+
+            result_ = entry.first;
+            return entry.second;
+        }
+
+    private:
+        std::stack<stack_entry> exprStack_;
+        Constant result_;
     };
+
+    inline void ComplexityEstimationVisitor::pop(stack_entry& fst) {
+        assert(exprStack_.size() >= 1);
+        fst = exprStack_.top();
+        exprStack_.pop();
+    }
+
+    inline void ComplexityEstimationVisitor::pop2(stack_entry& fst, stack_entry& snd) {
+        assert(exprStack_.size() >= 2);
+        fst = exprStack_.top();
+        exprStack_.pop();
+        snd = exprStack_.top();
+        exprStack_.pop();
+    }
+
+    inline void ComplexityEstimationVisitor::pop3(stack_entry& fst, stack_entry& snd, stack_entry& trd) {
+        assert(exprStack_.size() >= 3);
+        fst = exprStack_.top();
+        exprStack_.pop();
+        snd = exprStack_.top();
+        exprStack_.pop();
+        trd = exprStack_.top();
+        exprStack_.pop();
+    }
+
+    inline void ComplexityEstimationVisitor::evalBinExpr(BinaryExpression const& bin, stack_entry& fst, stack_entry& snd) {
+        visitBinaryExpr(bin);
+        pop2(snd, fst);
+    }
+
+    inline void ComplexityEstimationVisitor::evalTernExpr(TernaryExpression const& tern, stack_entry& fst, stack_entry& snd,
+            stack_entry& trd) {
+        visitTernaryExpr(tern);
+        pop3(trd, snd, fst);
+    }
 }
--- src/lib/ComplexityEstimationVisitor.cpp
+++ src/lib/ComplexityEstimationVisitor.cpp
@@ -1,18 +1,14 @@
-/* 
- * File:   ComplexityEstimationVisitor.cpp
- * Author: niklas
- * 
- * Created on 24. Juni 2015, 21:15
- */
+#include "../crave/ir/visitor/ComplexityEstimationVisitor.hpp"
 
-#include "ComplexityEstimationVisitor.hpp"
+namespace crave {
 
-ComplexityEstimationVisitor::ComplexityEstimationVisitor() {
-}
+    ComplexityEstimationVisitor::ComplexityEstimationVisitor() {
+    }
 
-ComplexityEstimationVisitor::ComplexityEstimationVisitor(const ComplexityEstimationVisitor& orig) {
-}
+    ComplexityEstimationVisitor::ComplexityEstimationVisitor(const ComplexityEstimationVisitor& orig) {
+    }
 
-ComplexityEstimationVisitor::~ComplexityEstimationVisitor() {
+    ComplexityEstimationVisitor::~ComplexityEstimationVisitor() {
+    }
 }
 
