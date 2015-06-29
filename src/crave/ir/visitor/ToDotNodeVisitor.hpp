f08466b5749a130cdecd2f96fe064a847ca12224
Niklas <nbruns@tzi.de>
2015-06-29 14:37:58 +0200

    remove redundancy for unary Operations

--- src/crave/ir/visitor/ToDotNodeVisitor.hpp
+++ src/crave/ir/visitor/ToDotNodeVisitor.hpp
@@ -70,6 +70,17 @@ class ToDotVisitor : public NodeVisitor {
   }
   visitBinaryExpr(o);
 }
+ template <typename T> 
+  void unaryOperator(T const &o ,std::string name)
+{
+    if (putNode(&o)) {
+    visitNode(o);
+    out_ << " [label=\"";
+    visitUnaryOpr(o);
+    out_ << ": "+name+"\"]" << std::endl;
+  }
+  visitUnaryExpr(o);
+}
   
  private:
   std::ostream& out_;
