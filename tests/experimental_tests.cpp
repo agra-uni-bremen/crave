20c22bca725c671a8f8faa4312ce45a73dca6cf4
Niklas Bruns <nbruns@tzi.de>
2015-07-07 18:18:00 +0200

    delete unnecessary variables

--- tests/experimental_tests.cpp
+++ tests/experimental_tests.cpp
@@ -8,38 +8,42 @@ struct Context_Fixture {
 
 #include "test_CoverageSampling.cpp"
 #include "test_ObjectHierarchy.cpp"
+
 #include "../src/crave/experimental/Variable.hpp"
+#include "../src/crave/experimental/Coverage.hpp"
 #include "../src/crave/experimental/Constraint.hpp"
+#include <boost/test/unit_test.hpp>
+
+using crave::crv_sequence_item;
+using crave::crv_variable;
+using crave::crv_object_name;
+using crave::crv_constraint;
 
 class Item : public crv_sequence_item {
  public:
-  Item() : crv_sequence_item(), sumName("sum"),anonymName("anonym"),productName("product"),xName("x")  {
+  Item() : crv_sequence_item()  {
       this->children_.push_back(&a);
       this->children_.push_back(&b);
-      sum = new crv_constraint(sumName);
+      sum = new crv_constraint("sum");
       sum->operator() (a() + b() == 4);
       this->children_.push_back(sum);
       
-      product = new crv_constraint(productName);
+      product = new crv_constraint("product");
       product->operator() (a() * b() == 4);
       this->children_.push_back(product);
       
-      anonym = new crv_constraint(anonymName);
+      anonym = new crv_constraint("anonym");
       anonym->operator() (a() < 10);
       anonym->operator() (b() < 10);
       this->children_.push_back(anonym);
       
-      x = new crv_constraint(xName);
+      x = new crv_constraint("x");
       x->operator() (a() != 2);
       this->children_.push_back(x);
   }
   crv_variable<uint> a;
   crv_variable<uint> b;
   
-  crv_object_name sumName;
-  crv_object_name anonymName;
-  crv_object_name productName;
-  crv_object_name xName;
   crv_constraint *sum;
   crv_constraint *anonym;
   crv_constraint *product;
@@ -96,3 +100,4 @@ BOOST_AUTO_TEST_CASE(t1) {
   std::cout << it << std::endl;
   BOOST_REQUIRE(it.a == 2 && it.b == 2);
 }
+ 
