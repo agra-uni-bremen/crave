2c11736c09792c740c13a7e3e5fab29988c42cc4
Niklas <nbruns@tzi.de>
2015-07-08 12:41:57 +0200

    change Item

--- tests/experimental_tests.cpp
+++ tests/experimental_tests.cpp
@@ -19,82 +19,74 @@ using crave::crv_variable;
 using crave::crv_object_name;
 using crave::crv_constraint;
 
-class Item : public crv_sequence_item {
- public:
-  Item() : crv_sequence_item()  {
-      this->children_.push_back(&a);
-      this->children_.push_back(&b);
-      sum = new crv_constraint("sum");
-      sum->operator() (a() + b() == 4);
-      this->children_.push_back(sum);
+struct Item : public crv_sequence_item{
+      crv_variable<unsigned int> a { "a" };
+      crv_variable<unsigned int> b { "b" };
       
-      product = new crv_constraint("product");
-      product->operator() (a() * b() == 4);
-      this->children_.push_back(product);
+      crv_constraint sum {"sum"};
+      crv_constraint product {"product"};
+      crv_constraint anonym {"anonym"};
+      crv_constraint x {"x"};
       
-      anonym = new crv_constraint("anonym");
-      anonym->operator() (a() < 10);
-      anonym->operator() (b() < 10);
-      this->children_.push_back(anonym);
-      
-      x = new crv_constraint("x");
-      x->operator() (a() != 2);
-      this->children_.push_back(x);
-  }
-  crv_variable<uint> a;
-  crv_variable<uint> b;
-  
-  crv_constraint *sum;
-  crv_constraint *anonym;
-  crv_constraint *product;
-  crv_constraint *x;
-  friend ostream& operator<<(ostream& os, const Item& it) {
-    os << it.a << " " << it.b;
+      Item(crv_object_name)
+      {
+          sum = {a() + b() == 4};
+          product = {a() * b() == 4};
+          anonym = {a() < 10,b() < 10};
+          x = {a() != 2};
+          sum.activate();
+          product.activate();
+          anonym.activate();
+          x.activate();
+      }
+      friend ostream& operator<<(ostream& os, const Item& it) {
+      os << it.a << " " << it.b;
     return os;
   }
 };
 
+
 BOOST_AUTO_TEST_CASE(t1) {
-  Item it;
+  Item it("Item");
 
   BOOST_REQUIRE(!it.randomize());
-  BOOST_REQUIRE(it.sum->active());
-  BOOST_REQUIRE(it.product->active());
-  BOOST_REQUIRE(it.x->active());
+  BOOST_REQUIRE(it.sum.active());
+  BOOST_REQUIRE(it.product.active());
+  BOOST_REQUIRE(it.x.active());
 
   
-  it.sum->deactivate();
+  it.sum.deactivate();
   BOOST_REQUIRE(it.randomize());
-  BOOST_REQUIRE(!it.sum->active());
-  BOOST_REQUIRE(it.product->active());
-  BOOST_REQUIRE(it.x->active());
+  BOOST_REQUIRE(!it.sum.active());
+  BOOST_REQUIRE(it.product.active());
+  BOOST_REQUIRE(it.x.active());
   std::cout << it << std::endl;
 
- it.product->deactivate();
+ it.product.deactivate();
   BOOST_REQUIRE(it.randomize());
-  BOOST_REQUIRE(!it.sum->active());
-  BOOST_REQUIRE(!it.product->active());
-  BOOST_REQUIRE(it.x->active());
+  BOOST_REQUIRE(!it.sum.active());
+  BOOST_REQUIRE(!it.product.active());
+  BOOST_REQUIRE(it.x.active());
   std::cout << it << std::endl;
 
-  it.sum->activate();
+  it.sum.activate();
   BOOST_REQUIRE(it.randomize());
-  BOOST_REQUIRE(it.sum->active());
-  BOOST_REQUIRE(!it.product->active());
-  BOOST_REQUIRE(it.x->active());
+  BOOST_REQUIRE(it.sum.active());
+  BOOST_REQUIRE(!it.product.active());
+  BOOST_REQUIRE(it.x.active());
   std::cout << it << std::endl;
 
-  it.product->activate();
+  it.product.activate();
   BOOST_REQUIRE(!it.randomize());
-  BOOST_REQUIRE(it.sum->active());
-  BOOST_REQUIRE(it.product->active());
-  BOOST_REQUIRE(it.x->active());
+  BOOST_REQUIRE(it.sum.active());
+  BOOST_REQUIRE(it.product.active());
+  BOOST_REQUIRE(it.x.active());
 
-  it.x->deactivate();
+  it.x.deactivate();
   BOOST_REQUIRE(it.randomize());
-  BOOST_REQUIRE(it.sum->active());
-  BOOST_REQUIRE(it.product->active());
-  BOOST_REQUIRE(!it.x->active());
+  BOOST_REQUIRE(it.sum.active());
+  BOOST_REQUIRE(it.product.active());
+  BOOST_REQUIRE(!it.x.active());
 
   std::cout << it << std::endl;
   BOOST_REQUIRE(it.a == 2 && it.b == 2);
