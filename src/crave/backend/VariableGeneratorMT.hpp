9cfe55d5978ed2befa969c4e91ee0d39818bd08e
Hoang M. Le <hle@informatik.uni-bremen.de>
2015-07-14 16:51:09 +0200

    refactored

--- src/crave/backend/VariableGeneratorMT.hpp
+++ src/crave/backend/VariableGeneratorMT.hpp
@@ -1,24 +1,19 @@
-#ifndef VARIABLEGENERATORMT_HPP
-#define	VARIABLEGENERATORMT_HPP
+#pragma once
 
 #include "VariableGeneratorType.hpp"
 
 namespace crave {
+class VariableGeneratorMT : public VariableGenerator {
+ public:
+  explicit VariableGeneratorMT(VariableContainer const& vcon);
+  virtual ~VariableGeneratorMT() {}
 
-    class VariableGeneratorMT : public VariableGenerator {
-    public:
-        explicit VariableGeneratorMT(VariableContainer const& vcon);
-        virtual ~VariableGeneratorMT(){}
+  virtual void reset(std::vector<ConstraintPartition>& partitions);
+  virtual bool solve();
 
-        virtual void reset(std::vector<ConstraintPartition>& partitions);
-        virtual bool solve();
-    private:
-        void createNewSolver(ConstraintPartition & partition,unsigned int index);
-        void solve(VarSolverPtr vs,unsigned int index);
-        boost::shared_ptr<VariableSolver>* var_solv_array;
-        bool *result_array;
-
-    };
+ private:
+  void createNewSolver(ConstraintPartition& partition, unsigned int index);
+  void solve(VarSolverPtr vs);
+  bool result_;
+};
 }
-#endif	/* VARIABLEGENERATORMT_HPP */
-
--- src/lib/VariableGeneratorMT.cpp
+++ src/lib/VariableGeneratorMT.cpp
@@ -4,54 +4,36 @@
 #include <boost/shared_ptr.hpp>
 
 namespace crave {
-VariableGeneratorMT::VariableGeneratorMT(VariableContainer const& vcon) : VariableGenerator(vcon){
-}
+VariableGeneratorMT::VariableGeneratorMT(VariableContainer const& vcon) : VariableGenerator(vcon) {}
 
-void VariableGeneratorMT::createNewSolver(ConstraintPartition & partition,unsigned int index)
-{
-    var_solv_array[index] = boost::make_shared<VariableDefaultSolver>(var_ctn_,partition);
+void VariableGeneratorMT::createNewSolver(ConstraintPartition& partition, unsigned int index) {
+  solvers_[index] = boost::make_shared<VariableDefaultSolver>(var_ctn_, partition);
 }
-
 
 void VariableGeneratorMT::reset(std::vector<ConstraintPartition>& partitions) {
-  var_solv_array = new VarSolverPtr[partitions.size()];
-  
+  solvers_.resize(partitions.size());
   boost::thread_group threads;
-      for(int i=0;i<partitions.size();i++)
-      {
-          boost::thread *thread(new boost::thread(boost::bind(&VariableGeneratorMT::createNewSolver,this, boost::ref(partitions.at(i)),i)));  
-          threads.add_thread(thread);
-      }
-  threads.join_all();
-  solvers_.clear();
-  for(int i=0;i<partitions.size();i++)
-  {
-      solvers_.push_back(var_solv_array[i]);
-  }
-  delete []var_solv_array;
-}
-
-void VariableGeneratorMT::solve(VarSolverPtr vs, unsigned int index)
-{
-    result_array[index] = vs->solve();
-}
-
-bool VariableGeneratorMT::solve() {
-  result_array = new bool[this->solvers_.size()];
-  boost::thread_group threads;
-  for(unsigned int i=0;i<this->solvers_.size();i++)
-  {
-    boost::thread *thread(new boost::thread(boost::bind(&VariableGeneratorMT::solve,this, this->solvers_.at(i),i)));
+  for (unsigned i = 0; i < partitions.size(); i++) {
+    boost::thread* thread(
+        new boost::thread(boost::bind(&VariableGeneratorMT::createNewSolver, this, boost::ref(partitions.at(i)), i)));
     threads.add_thread(thread);
   }
   threads.join_all();
-  bool result = true;
-  for(unsigned int i=0;i<this->solvers_.size();i++)
-  {
-      result &= result_array[i];
-  }
-  delete [] result_array;
-  return result;
 }
 
+void VariableGeneratorMT::solve(VarSolverPtr vs) {
+  if (!result_) return;
+  if (!vs->solve()) result_ = false;
+}
+
+bool VariableGeneratorMT::solve() {
+  result_ = true;
+  boost::thread_group threads;
+  for (unsigned i = 0; i < solvers_.size(); i++) {
+    boost::thread* thread(new boost::thread(boost::bind(&VariableGeneratorMT::solve, this, solvers_.at(i))));
+    threads.add_thread(thread);
+  }
+  threads.join_all();
+  return result_;
+}
 }
