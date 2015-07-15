1b4e7ee951f8ec79b3cf310b746be2924a82dc85
Niklas <nbruns@tzi.de>
2015-07-15 21:48:20 +0200

    refactored mt19937Manager

--- src/crave/mt19937Manager.hpp
+++ src/crave/mt19937Manager.hpp
@@ -7,31 +7,10 @@ class mt19937Manager {
 public:
     mt19937Manager(unsigned int seed);
     virtual ~mt19937Manager();
-
-    void set_global_seed(unsigned int s) {
-        seed_ = s;
-        actualizeSeed();
-    }
-
-    boost::mt19937* get() {
-        unsigned int processId = 0;
-        if (randomMap_.count(processId)) { //constains
-            return randomMap_.at(processId);
-        }
-        boost::mt19937* rnd = new boost::mt19937(seed_);
-        randomMap_.insert(std::make_pair(processId, rnd));
-        return rnd;
-    }
-
+    void set_global_seed(unsigned int s);
+    boost::mt19937* get();
 private:
-
-    void actualizeSeed() {
-        for (auto tuple : randomMap_) {
-            tuple.second->seed(seed_);
-        }
-    }
+    void actualizeSeed();
     std::map<unsigned int, boost::mt19937*> randomMap_;
     unsigned int seed_;
-
-
 };
--- src/lib/mt19937Manager.cpp
+++ src/lib/mt19937Manager.cpp
@@ -1,3 +1,4 @@
+// Copyright 2014 The CRAVE developers. All rights reserved.//
 #include "../crave/mt19937Manager.hpp"
 #include <ctime>
 
@@ -9,4 +10,25 @@ mt19937Manager::~mt19937Manager() {
         delete tuple.second;
     }
 }
+
+void mt19937Manager::set_global_seed(unsigned int s) {
+    seed_ = s;
+    actualizeSeed();
+}
+
+boost::mt19937* mt19937Manager::get() {
+        unsigned int processId = 0;
+        if (randomMap_.count(processId)) { //constains
+            return randomMap_.at(processId);
+        }
+        boost::mt19937* rnd = new boost::mt19937(seed_);
+        randomMap_.insert(std::make_pair(processId, rnd));
+        return rnd;
+    }
+
+void mt19937Manager::actualizeSeed() {
+        for (auto tuple : randomMap_) {
+            tuple.second->seed(seed_);
+        }
+    }
 
