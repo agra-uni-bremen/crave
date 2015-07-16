e148677af5ee97804ab8a14d3804bd6efb45d929
Niklas <nbruns@tzi.de>
2015-07-16 21:24:35 +0200

    add systemc process differ

--- src/lib/mt19937Manager.cpp
+++ src/lib/mt19937Manager.cpp
@@ -1,11 +1,13 @@
 // Copyright 2014 The CRAVE developers. All rights reserved.//
 #include "../crave/mt19937Manager.hpp"
+#include <systemc.h>
 #include <ctime>
 
 mt19937Manager::mt19937Manager(unsigned int seed) : seed_(seed) {
 }
 
 mt19937Manager::~mt19937Manager() {
+
     for (auto tuple : randomMap_) {
         delete tuple.second;
     }
@@ -17,18 +19,23 @@ void mt19937Manager::set_global_seed(unsigned int s) {
 }
 
 boost::mt19937* mt19937Manager::get() {
-        unsigned int processId = 0;
-        if (randomMap_.count(processId)) { //constains
-            return randomMap_.at(processId);
-        }
-        boost::mt19937* rnd = new boost::mt19937(seed_);
-        randomMap_.insert(std::make_pair(processId, rnd));
-        return rnd;
+    unsigned int processId = 0; //if no systemc process, only 1 randomgenerator
+    sc_process_b* process = sc_core::sc_get_current_process_b();
+    if (process != NULL) {
+        processId = process->proc_id;
     }
 
-void mt19937Manager::actualizeSeed() {
-        for (auto tuple : randomMap_) {
-            tuple.second->seed(seed_);
-        }
+    if (randomMap_.count(processId)) { //constains
+        return randomMap_.at(processId);
     }
+    boost::mt19937* rnd = new boost::mt19937(seed_);
+    randomMap_.insert(std::make_pair(processId, rnd));
+    return rnd;
+}
+
+void mt19937Manager::actualizeSeed() {
+    for (auto tuple : randomMap_) {
+        tuple.second->seed(seed_);
+    }
+}
 
