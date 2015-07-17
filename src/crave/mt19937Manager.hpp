ce0f20f7083f8de15ece55efc5a07145f21050b9
Hoang M. Le <hle@informatik.uni-bremen.de>
2015-07-17 11:03:32 +0200

    updated mt199937Manager

--- src/crave/mt19937Manager.hpp
+++ src/crave/mt19937Manager.hpp
@@ -4,13 +4,15 @@
 #include <boost/random.hpp>
 
 class mt19937Manager {
-public:
-    mt19937Manager(unsigned int seed);
-    virtual ~mt19937Manager();
-    void set_global_seed(unsigned int s);
-    boost::mt19937* get();
-private:
-    void actualizeSeed();
-    std::map<unsigned int, boost::mt19937*> randomMap_;
-    unsigned int seed_;
+ public:
+  mt19937Manager(unsigned int seed);
+  virtual ~mt19937Manager();
+  void set_global_seed(unsigned int s);
+  boost::mt19937* get();
+
+ private:
+  typedef std::map<unsigned int, boost::mt19937*> random_map_t;
+  random_map_t randomMap_;
+  boost::mt19937 default_rng_;
+  unsigned int seed_;
 };
--- src/lib/mt19937Manager.cpp
+++ src/lib/mt19937Manager.cpp
@@ -1,41 +1,36 @@
 // Copyright 2014 The CRAVE developers. All rights reserved.//
 #include "../crave/mt19937Manager.hpp"
-#include <systemc.h>
-#include <ctime>
+#include <boost/foreach.hpp>
 
-mt19937Manager::mt19937Manager(unsigned int seed) : seed_(seed) {
-}
+mt19937Manager::mt19937Manager(unsigned int seed) : seed_(seed), default_rng_(seed) {}
 
 mt19937Manager::~mt19937Manager() {
-
-    for (auto tuple : randomMap_) {
-        delete tuple.second;
-    }
+  BOOST_FOREACH(random_map_t::value_type & entry, randomMap_) { delete entry.second; }
 }
 
 void mt19937Manager::set_global_seed(unsigned int s) {
-    seed_ = s;
-    actualizeSeed();
+  seed_ = s;
+  default_rng_.seed(s);
 }
 
+#ifndef WITH_SYSTEMC
+
+boost::mt19937* mt19937Manager::get() { return &default_rng_; }
+
+#else
+
+#include <sysc/kernel/sc_simcontext.h>
 boost::mt19937* mt19937Manager::get() {
-    unsigned int processId = 0; //if no systemc process, only 1 randomgenerator
-    sc_process_b* process = sc_core::sc_get_current_process_b();
-    if (process != NULL) {
-        processId = process->proc_id;
-    }
-
-    if (randomMap_.count(processId)) { //constains
-        return randomMap_.at(processId);
-    }
-    boost::mt19937* rnd = new boost::mt19937(seed_);
-    randomMap_.insert(std::make_pair(processId, rnd));
-    return rnd;
+  sc_core::sc_process_b* process = sc_core::sc_get_current_process_b();
+  if (!process) return &default_rng_;
+  unsigned int processId = process->proc_id;
+  if (randomMap_.count(processId)) {  // constains
+    return randomMap_.at(processId);
+  }
+  boost::mt19937* rnd = new boost::mt19937(seed_ + 19937 * processId);
+  randomMap_.insert(std::make_pair(processId, rnd));
+  return rnd;
 }
 
-void mt19937Manager::actualizeSeed() {
-    for (auto tuple : randomMap_) {
-        tuple.second->seed(seed_);
-    }
-}
+#endif
 
