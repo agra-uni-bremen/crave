7962ce302ac87863d87c6f9992b4f9e078bca587
Niklas Bruns <nbruns@fb3.uni-bremen.de>
2016-01-22 20:00:56 +0100

    add include

--- tests/experimental_tests.cpp
+++ tests/experimental_tests.cpp
@@ -6,12 +6,13 @@
 struct Context_Fixture {
   Context_Fixture() {}
 };
-/*
 #include "test_CoverageSampling.cpp"
 #include "test_ExperimentalObjectHierarchy.cpp"
 #include "test_ExperimentalConstraintManagement.cpp"
 #include "test_ExperimentalOperators.cpp"
 #include "test_ExperimentalUserConstraint.cpp"
 #include "test_ExperimentalContext.cpp"
-#include "test_ExperimentalRandom_Object.cpp"*/
+#include "test_ExperimentalRandom_Object.cpp"
 #include "test_ExperimentalDistribution.cpp"
+#include "test_ExperimentalEvaluations.cpp"
+#include "test_ExperimentalVector_Constraint.cpp"
