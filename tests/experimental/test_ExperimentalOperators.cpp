8be8b65ca02beec5a01b203075f540762980a1f8
Niklas Bruns <nbruns@fb3.uni-bremen.de>
2016-02-28 23:07:39 +0100

    decrese iterations shiftleft to 420

--- tests/experimental/test_ExperimentalOperators.cpp
+++ tests/experimental/test_ExperimentalOperators.cpp
@@ -494,7 +494,7 @@ BOOST_AUTO_TEST_CASE(shiftleft) {
   VariableDefaultSolver::bypass_constraint_analysis = true;
   s_shiftleft item("item");
   int count = 0;
-  while (item.randomize() && ++count < 450) {
+  while (item.randomize() && ++count < 420) {
     unsigned av = item.a;
     unsigned bv = item.b;
     unsigned r = av << bv;
