ca0c447e11cd93ba9955fc4b8b8586a628850c5c
Niklas Bruns <nbruns@fb3.uni-bremen.de>
2016-04-26 20:47:44 +0200

    add debug output

--- src/lib/ConstraintManager.cpp
+++ src/lib/ConstraintManager.cpp
@@ -4,7 +4,7 @@
 #include "../crave/ir/visitor/ComplexityEstimationVisitor.hpp"
 
 #include <boost/make_shared.hpp>
-
+#include <iostream>
 #include <glog/logging.h>
 
 namespace crave {
@@ -20,8 +20,11 @@ ostream& operator<<(ostream& os, const ConstraintManager& set) {
 template std::ostream& operator<<<std::ostream>(std::ostream& os, const ConstraintManager& set);
 
 ConstraintManager::ConstraintManager() : changed_(false) {
+  std::cerr << "ConstruktorCall ConstraintManager" << std::endl;
   static unsigned ID = 0;
   id_ = ++ID;
+  std::cerr << "ID: " << &ID << std::endl;
+  std::cerr << "id_" << &id_ << std::endl;
 }
 
 bool ConstraintManager::enableConstraint(std::string const& key) {
@@ -69,16 +72,33 @@ void ConstraintManager::resetChanged() { changed_ = false; }
 
 ConstraintPtr ConstraintManager::makeConstraint(std::string const& name, int c_id, NodePtr n, Context* ctx,
                                                 bool const soft, bool const cover) {
+  std::cout << "name: " << name << std::endl;
+  std::cout << "c_id: " <<c_id << std::endl;
+  std::cout << "n: " << n << std::endl;
+  std::cout << "ctx: " << ctx << std::endl;
+  std::cout << "soft: " << soft << std::endl;
+  std::cout << "cover: " << cover << std::endl;
+  std::cout << "constr_map_:" << &constr_map_ << std::endl;
+
+  std::cout << "id_:" << &id_ << std::endl;
+  
+  std::cout << "hans-(-2)" << std::endl;
   LOG(INFO) << "New " << (soft ? "soft " : "") << (cover ? "cover " : "") << "constraint " << name << " in set " << id_;
 
+  std::cout << "hans-(-1)" << std::endl;
+  
   if (constr_map_.find(name) != constr_map_.end()) {
     throw std::runtime_error(name + "Constraint already exists.");
   }
+
+  std::cout << "hans-0" << std::endl;
 
   GetSupportSetVisitor gssv;
   n->visit(&gssv);
 
   ConstraintPtr c;
+
+  std::cout << "hans-1" << std::endl;
 
   if (boost::dynamic_pointer_cast<ForEach>(n) != 0) {
     c = boost::make_shared<UserVectorConstraint>(c_id, n, name, gssv.getSupportVars(), false, soft, cover);
@@ -88,9 +108,12 @@ ConstraintPtr ConstraintManager::makeConstraint(std::string const& name, int c_i
     c = boost::make_shared<UserConstraint>(c_id, n, name, gssv.getSupportVars(), soft, cover);
   }
 
+  std::cout << "hans-2" << std::endl;
+
   assert(!c->isSoft() || !c->isCover());              // soft cover constraint not defined/supported yet
   assert(!c->isVectorConstraint() || !c->isCover());  // cover vector constraint not defined/supported yet
 
+  std::cout << "hans-3" << std::endl;
   if (!c->isVectorConstraint()) {
     ComplexityEstimationVisitor cev;
     c->complexity_ = cev.getComplexityEstimation(*n);
