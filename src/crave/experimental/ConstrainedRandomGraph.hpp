b53918b08b77f33ea8c9747df1a89f0be726dd88
Niklas Bruns <nbruns@fb3.uni-bremen.de>
2016-05-11 10:00:48 +0200

    remove unused header

--- src/crave/experimental/ConstrainedRandomGraph.hpp
+++ src/crave/experimental/ConstrainedRandomGraph.hpp
@@ -1,15 +1,7 @@
 #pragma once
 
-#include <iostream>
-#include <string>
-#include <set>
-#include <functional>
-
-#include <boost/shared_ptr.hpp>
-
 #include "graph/Node.hpp"
 #include "graph/Rule.hpp"
 #include "graph/RuleContext.hpp"
-
 
 #define NAMED_RULE(name) crave::graph::rule_type name = {crave::graph::Rule(#name)};
