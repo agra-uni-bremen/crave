0f71ace640ad5aa1187efede93dbc71f083cc9c9
Niklas Bruns <nbruns@fb3.uni-bremen.de>
2016-05-11 12:00:18 +0200

    move functions to SequenceItem.cpp

--- src/crave/experimental/SequenceItem.hpp
+++ src/crave/experimental/SequenceItem.hpp
@@ -11,21 +11,13 @@ namespace crave
 {
  class crv_sequence_item : public crv_object {
  public:
-  crv_sequence_item() : gen_(), built_(false), cloned_(false) {}
+  crv_sequence_item();
 
-  crv_sequence_item(crv_sequence_item const & other) : crv_object(other), gen_(), built_(false), cloned_(true) {}
+  crv_sequence_item(crv_sequence_item const & other);
 
-  std::string obj_kind() const override final { return "crv_sequence_item"; }
+  std::string obj_kind() const override final;
 
-  bool randomize() override {
-    assert(!cloned_ && "cloned crv_sequence_item cannot be randomized");
-    if (!built_) {
-      gen_ = std::make_shared<Generator>();
-      recursive_build(*gen_);
-      built_ = true;
-    }
-    return gen_->nextCov();
-  }
+  bool randomize() override;
 
   template <typename... Exprs>
   bool randomize_with(Exprs... exprs) {
@@ -38,17 +30,10 @@ namespace crave
     return rand_with_gen_->next();
   }
 
-  void goal(crv_covergroup& group) {
-    for (auto e : group.bound_var_expr_list()) (*gen_)(e);
-    for (auto e : group.uncovered_as_list()) gen_->cover(e);
-  }
+  void goal(crv_covergroup& group);
 
  protected:
-  void request_rebuild() override {
-    built_ = false;
-    gen_.reset();
-    crv_object::request_rebuild();
-  }
+  void request_rebuild();
 
   std::shared_ptr<Generator> gen_;
   std::shared_ptr<Generator> rand_with_gen_;
