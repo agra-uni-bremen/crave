5219f906fc1ae22ef47d1a652a15fb2e5b7522b4
Niklas Bruns <nbruns@fb3.uni-bremen.de>
2016-05-11 11:03:48 +0200

    move constraint_base to own file

--- src/crave/experimental/Constraint.hpp
+++ src/crave/experimental/Constraint.hpp
@@ -4,43 +4,13 @@
 
 #include "Object.hpp"
 #include "Expression.hpp"
+#include "ConstraintBase.hpp"
 
 #include <glog/logging.h>
 
 #include <string>
 
 namespace crave {
-
-class crv_constraint_base : public crv_object {
- protected:
-  crv_constraint_base() : active_(true) {}
-
- public:
-  virtual expression_list const& expr_list() const = 0;
-
-  std::string obj_kind() const override final { return "crv_constraint"; }
-
-  void activate() {
-    if (!active_) {
-      active_ = true;
-      request_rebuild();
-    }
-  }
-
-  void deactivate() {
-    if (active_) {
-      active_ = false;
-      request_rebuild();
-    }
-  }
-
-  bool active() const { return active_; }
-
-  virtual bool soft() const = 0;
-
- protected:
-  bool active_;
-};
 
 enum ConstraintType { hard, soft };
 
