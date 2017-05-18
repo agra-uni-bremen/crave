f00ad9b259d20f2be0056bcac2ea91e632eef27c
Hoang M. Le <hle@informatik.uni-bremen.de>
2017-05-18 15:23:51 +0200

    update tests and examples for the API change

--- tests/experimental/test_CoverageSampling.cpp
+++ tests/experimental/test_CoverageSampling.cpp
@@ -9,6 +9,14 @@ using namespace crave;
 
 BOOST_FIXTURE_TEST_SUITE(CoverageSampling, Context_Fixture)
 
+template<typename T>
+void sample_from_list(crv_covergroup& cg, crv_variable<T>& v, std::vector<T>&& values) {
+  for (T val : values) {
+    v = val;
+    cg.sample();
+  }
+}
+
 struct my_covergroup : public crv_covergroup {
   crv_variable<unsigned> a;
   crv_variable<unsigned> b;
@@ -17,92 +25,8 @@ struct my_covergroup : public crv_covergroup {
   crv_coverpoint cp2{"coverpoint2"};
 
   my_covergroup(crv_object_name) {
-    cp1.bins() = {crv_bin(a() == 0, 10), crv_bin(a() == 1, 10), crv_bin(a() == 2, 5)};
-    cp2.bins() = {crv_bin(a() == 0, 10), crv_bin(a() == 1, 10), crv_bin(a() == 2, 5)};
-  }
-};
-
-struct my_transitionCovergroup_next : public crv_covergroup {
-  crv_variable<unsigned> a;
-
-  crv_coverpoint cp1{"coverpoint1"};
-
-  my_transitionCovergroup_next(crv_object_name) {
-      //1
-      cp1.start_with(a() == 1);
-      cp1.allBins().back()->hit_minimum(4);
-      //1 => 2
-      cp1.start_with(a() == 1)->next(a() == 2);
-      cp1.allBins().back()->hit_minimum(1);
-      //0 => 1,2[*2] => 0
-      cp1.start_with(a() == 0)->next<2>(a() == 1 || a() == 2)->next(a() == 0);
-      cp1.allBins().back()->hit_minimum(1);
-      //0 [*2:3] => 1
-      cp1.start_with<2,3>(a() == 0)->next(a() == 1);
-      cp1.allBins().back()->hit_minimum(2);
-  }
-};
-
-struct my_transitionCovergroup_consNext : public crv_covergroup {
-  crv_variable<unsigned> a;
-
-  crv_coverpoint cp1{"coverpoint1"};
-
-  my_transitionCovergroup_consNext(crv_object_name) {
-      //1
-      cp1.start_with_consecutive(a() == 1);
-      cp1.allBins().back()->hit_minimum(4);
-      //1 [->3]
-      cp1.start_with_consecutive<3>(a() == 1);
-      cp1.allBins().back()->hit_minimum(2);
-      //0 [->2] => 1
-      cp1.start_with_consecutive<2>(a() == 0)->next(a() == 1);
-      cp1.allBins().back()->hit_minimum(3);
-      //0 => 1[->2:4] => 0
-      cp1.start_with(a() == 0)->consecutive_next<2,4>(a() == 1)->next(a() == 0);
-      cp1.allBins().back()->hit_minimum(1);
-  }
-};
-
-struct my_transitionCovergroup_nconsNext : public crv_covergroup {
-  crv_variable<unsigned> a;
-
-  crv_coverpoint cp1{"coverpoint1"};
-
-  my_transitionCovergroup_nconsNext(crv_object_name) {
-      //1
-      cp1.start_with_nonconsecutive(a() == 1);
-      cp1.allBins().back()->hit_minimum(4);
-      //1 [= 3]
-      cp1.start_with_nonconsecutive<3>(a() == 1);
-      cp1.allBins().back()->hit_minimum(2);
-      //0 [= 2] => 2
-      cp1.start_with_nonconsecutive<2>(a() == 0)->next(a() == 2);
-      cp1.allBins().back()->hit_minimum(1);
-      //0 => 1[= 2:4] => 0
-      cp1.start_with(a() == 0)->nonconsecutive_next<2,4>(a() == 1)->next(a() == 0);
-      cp1.allBins().back()->hit_minimum(2);
-  }
-};
-
-struct my_transitionCovergroup_overlap : public crv_covergroup {
-  crv_variable<unsigned> a;
-
-  crv_coverpoint cp1{"coverpoint1"};
-
-  my_transitionCovergroup_overlap(crv_object_name) {
-      //0 => 0
-      cp1.start_with<2>(a() == 0);
-      cp1.allBins().back()->hit_minimum(6);
-      //2 => 0[1:2]
-      cp1.start_with(a() == 2)->next<1,2>(a()==0);
-      cp1.allBins().back()->hit_minimum(2);
-      //0 => 1[->2]
-      cp1.start_with(a() == 0)->consecutive_next<2>(a() == 1);
-      cp1.allBins().back()->hit_minimum(1);
-      //0[*4] => 2 => 0[*2] => 1 => 0[*3] => 1
-      cp1.start_with<4>(a() == 0)->next(a() == 2)->next<2>(a() == 0)->next(a()==1)->next<3>(a() == 0)->next(a()==1);
-      cp1.allBins().back()->hit_minimum(1);
+    cp1.simple_bins() = {crv_bin(a() == 0, 10), crv_bin(a() == 1, 10), crv_bin(a() == 2, 5)};
+    cp2.simple_bins() = {crv_bin(a() == 0, 10), crv_bin(a() == 1, 10), crv_bin(a() == 2, 5)};
   }
 };
 
@@ -118,79 +42,118 @@ BOOST_AUTO_TEST_CASE(basic_test) {
   BOOST_REQUIRE(cg.covered());
 }
 
+struct my_transitionCovergroup_next : public crv_covergroup {
+  crv_variable<unsigned> a;
+
+  crv_coverpoint cp1{"coverpoint1"};
+
+  my_transitionCovergroup_next(crv_object_name) {
+      //1
+      cp1.start_with(a() == 1);
+      cp1.last_transition_bin().hit_minimum(4);
+      //1 => 2
+      cp1.start_with(a() == 1)->next(a() == 2);
+      cp1.last_transition_bin().hit_minimum(1);
+      //0 => 1,2[*2] => 0
+      cp1.start_with(a() == 0)->next<2>(a() == 1 || a() == 2)->next(a() == 0);
+      cp1.last_transition_bin().hit_minimum(1);
+      //0 [*2:3] => 1
+      cp1.start_with<2,3>(a() == 0)->next(a() == 1);
+      cp1.last_transition_bin().hit_minimum(2);
+  }
+};
+
 BOOST_AUTO_TEST_CASE(transition_next_test) {
   my_transitionCovergroup_next cg{"covergroup2"};
-  cg.a = 0; cg.sample();
-  cg.a = 1; cg.sample();
-  cg.a = 0; cg.sample();
-  cg.a = 1; cg.sample();
-  cg.a = 2; cg.sample();
-  
-  cg.a = 0; cg.sample();
-  cg.a = 0; cg.sample();
-  cg.a = 1; cg.sample();
-  cg.a = 0; cg.sample();
-  cg.a = 0; cg.sample();
-  cg.a = 0; cg.sample();
-  cg.a = 1; cg.sample();
+
+  sample_from_list(cg, cg.a, {0, 1, 0, 1, 2, 0, 0, 1, 0, 0, 0, 1});
 
   BOOST_REQUIRE(cg.covered());
 }
+
+struct my_transitionCovergroup_consNext : public crv_covergroup {
+  crv_variable<unsigned> a;
+
+  crv_coverpoint cp1{"coverpoint1"};
+
+  my_transitionCovergroup_consNext(crv_object_name) {
+      //1
+      cp1.start_with_consecutive(a() == 1);
+      cp1.last_transition_bin().hit_minimum(4);
+      //1 [->3]
+      cp1.start_with_consecutive<3>(a() == 1);
+      cp1.last_transition_bin().hit_minimum(2);
+      //0 [->2] => 1
+      cp1.start_with_consecutive<2>(a() == 0)->next(a() == 1);
+      cp1.last_transition_bin().hit_minimum(3);
+      //0 => 1[->2:4] => 0
+      cp1.start_with(a() == 0)->consecutive_next<2,4>(a() == 1)->next(a() == 0);
+      cp1.last_transition_bin().hit_minimum(1);
+  }
+};
 
 BOOST_AUTO_TEST_CASE(transition_cons_next_test) {
   my_transitionCovergroup_consNext cg{"covergroup3"};
-  cg.a = 0; cg.sample();
-  cg.a = 1; cg.sample();
-  cg.a = 0; cg.sample();
-  cg.a = 1; cg.sample();
-  cg.a = 2; cg.sample();
-  
-  cg.a = 0; cg.sample();
-  cg.a = 0; cg.sample();
-  cg.a = 1; cg.sample();
-  cg.a = 0; cg.sample();
-  cg.a = 0; cg.sample();
-  cg.a = 0; cg.sample();
-  cg.a = 1; cg.sample();
+
+  sample_from_list(cg, cg.a, {0, 1, 0, 1, 2, 0, 0, 1, 0, 0, 0, 1});
 
   BOOST_REQUIRE(cg.covered());
 }
+
+struct my_transitionCovergroup_nconsNext : public crv_covergroup {
+  crv_variable<unsigned> a;
+
+  crv_coverpoint cp1{"coverpoint1"};
+
+  my_transitionCovergroup_nconsNext(crv_object_name) {
+      //1
+      cp1.start_with_nonconsecutive(a() == 1);
+      cp1.last_transition_bin().hit_minimum(4);
+      //1 [= 3]
+      cp1.start_with_nonconsecutive<3>(a() == 1);
+      cp1.last_transition_bin().hit_minimum(2);
+      //0 [= 2] => 2
+      cp1.start_with_nonconsecutive<2>(a() == 0)->next(a() == 2);
+      cp1.last_transition_bin().hit_minimum(1);
+      //0 => 1[= 2:4] => 0
+      cp1.start_with(a() == 0)->nonconsecutive_next<2,4>(a() == 1)->next(a() == 0);
+      cp1.last_transition_bin().hit_minimum(2);
+  }
+};
 
 BOOST_AUTO_TEST_CASE(transition_ncons_next_test) {
   my_transitionCovergroup_nconsNext cg{"covergroup4"};
-  cg.a = 0; cg.sample();
-  cg.a = 1; cg.sample();
-  cg.a = 0; cg.sample();
-  cg.a = 1; cg.sample();
-  cg.a = 2; cg.sample();
-  
-  cg.a = 0; cg.sample();
-  cg.a = 0; cg.sample();
-  cg.a = 1; cg.sample();
-  cg.a = 0; cg.sample();
-  cg.a = 0; cg.sample();
-  cg.a = 0; cg.sample();
-  cg.a = 1; cg.sample();
+
+  sample_from_list(cg, cg.a, {0, 1, 0, 1, 2, 0, 0, 1, 0, 0, 0, 1});
 
   BOOST_REQUIRE(cg.covered());
 }
 
+struct my_transitionCovergroup_overlap : public crv_covergroup {
+  crv_variable<unsigned> a;
+
+  crv_coverpoint cp1{"coverpoint1"};
+
+  my_transitionCovergroup_overlap(crv_object_name) {
+      //0 => 0
+      cp1.start_with<2>(a() == 0);
+      cp1.last_transition_bin().hit_minimum(6);
+      //2 => 0[1:2]
+      cp1.start_with(a() == 2)->next<1,2>(a()==0);
+      cp1.last_transition_bin().hit_minimum(2);
+      //0 => 1[->2]
+      cp1.start_with(a() == 0)->consecutive_next<2>(a() == 1);
+      cp1.last_transition_bin().hit_minimum(1);
+      //0[*4] => 2 => 0[*2] => 1 => 0[*3] => 1
+      cp1.start_with<4>(a() == 0)->next(a() == 2)->next<2>(a() == 0)->next(a()==1)->next<3>(a() == 0)->next(a()==1);
+      cp1.last_transition_bin().hit_minimum(1);
+  }
+};
+
 BOOST_AUTO_TEST_CASE(transition_overlap_test) {
   my_transitionCovergroup_overlap cg{"covergroup5"};
-  cg.a = 0; cg.sample();
-  cg.a = 0; cg.sample();
-  cg.a = 0; cg.sample();
-  cg.a = 0; cg.sample();
-  
-  cg.a = 2; cg.sample();
-  cg.a = 0; cg.sample();
-  cg.a = 0; cg.sample();
-  
-  cg.a = 1; cg.sample();
-  cg.a = 0; cg.sample();
-  cg.a = 0; cg.sample();
-  cg.a = 0; cg.sample();
-  cg.a = 1; cg.sample();
+
+  sample_from_list(cg, cg.a, {0, 0, 0, 0, 2, 0, 0, 1, 0, 0, 0, 1});
 
   BOOST_REQUIRE(cg.covered());
 }
