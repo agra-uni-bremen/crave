7bb12ae0a987882151e4180029f1ee22c75d511f
Hoang M. Le <hle@informatik.uni-bremen.de>
2018-06-27 14:18:03 +0200

    add support for SystemC sc_bv wider than 64-bit

--- src/crave/experimental/SystemC.hpp
+++ src/crave/experimental/SystemC.hpp
@@ -10,9 +10,17 @@
 namespace crave {
 
 template <typename T>
-struct to_constant_expr<T, typename std::enable_if<is_sysc_dt<T>::value>::type> {
+struct to_constant_expr<T, typename std::enable_if<is_sysc_dt<T>::value && bitsize_traits<T>::value <= 64>::type> {
   Constant operator()(T value) {
     return Constant(value.to_uint64(), bitsize_traits<T>::value, crave::is_signed<T>::value);
+  }
+};
+
+template <typename T>
+struct to_constant_expr<T, typename std::enable_if<is_sysc_dt<T>::value && bitsize_traits<T>::value >= 65>::type> {
+  Constant operator()(T value) {
+    // XXX conversion over strings might not be portable
+    return Constant(unsigned_int<bitsize_traits<T>::value>(value.to_string(sc_dt::SC_HEX)), bitsize_traits<T>::value, crave::is_signed<T>::value);
   }
 };
 
@@ -20,7 +28,7 @@ struct to_constant_expr<T, typename std::enable_if<is_sysc_dt<T>::value>::type> 
  * crv_variable for SystemC data types
  */
 template <typename T>
-class crv_variable<T, typename std::enable_if<is_sysc_dt<T>::value>::type> : public crv_variable_base<T> {
+class crv_variable<T, typename std::enable_if<is_sysc_dt<T>::value && bitsize_traits<T>::value <= 64>::type> : public crv_variable_base<T> {
   CRV_VARIABLE_COMMON_CONSTRUCTORS(T);
   CRV_VARIABLE_ASSIGNMENT_INTERFACE(T);
   CRV_VARIABLE_ARITHMETIC_INTERFACE(T);
@@ -34,6 +42,33 @@ class crv_variable<T, typename std::enable_if<is_sysc_dt<T>::value>::type> : pub
   bool randomize() override {
     static distribution<uint64_t> dist;
     this->value = dist.nextValue();
+    return true;
+  }
+};
+
+template <typename T>
+class crv_variable<T, typename std::enable_if<is_sysc_dt<T>::value && bitsize_traits<T>::value >= 65>::type> : public crv_variable_base<T> {
+  CRV_VARIABLE_COMMON_CONSTRUCTORS(T);
+  CRV_VARIABLE_ASSIGNMENT_INTERFACE(T);
+  CRV_VARIABLE_ARITHMETIC_INTERFACE(T);
+  CRV_VARIABLE_BITWISE_INTERFACE(T);
+
+  static_assert(bitsize_traits<T>::value <= bitsize_traits<underlying_type>::value, "requested bit width is too big");
+
+ public:
+  /**
+   * randomize variable
+   * @return true if success
+   */
+  bool randomize() override {
+    using unsigned_type = unsigned_int<bitsize_traits<T>::value>;
+    static distribution<unsigned_type> dist;
+    unsigned_type val = dist.nextValue();
+    std::string bin(bitsize_traits<T>::value, '0');
+    for (unsigned i = 0; i < bin.length(); i++)
+      if ((val >> i) & 1) bin[bin.length() - i - 1] = '1';
+    // XXX conversion over strings might not be portable
+    this->value = bin.c_str();
     return true;
   }
 };
